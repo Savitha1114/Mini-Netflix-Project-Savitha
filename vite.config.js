a24b0693b3bd305d7ad4e2f2fb1598bdc7a6f526
S Savitha <savitha7975480774@gmail.com>
2025-06-22 20:15:33 +1100

    Add full Vue project source code

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  base: '/mini-netflix-vue/'  // MUST match your GitHub repo name
-})
 
+export default defineConfig({
+  base: '/Mini-Netflix-Project-Savitha/',
+  plugins: [vue()],
+})
